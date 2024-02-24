5b6fa39502e235a0c3dfe8bc730ce61b0c147c7e
dantvi <daniel.tving@gmail.com>
2024-02-24 14:27:49 +0100

    added names file

---
+++ names.js
@@ -0,0 +1,42 @@
+const babyNames = [
+    {
+        name: 'Melissa', 
+        gender: 'girl',
+    },
+    {
+        name: 'Richard', 
+        gender: 'boy',
+    },
+    {
+        name: 'Charlie', 
+        gender: 'boy',
+    },
+    {
+        name: 'Skye', 
+        gender: 'girl',
+    },
+    {
+        name: 'Lucas', 
+        gender: 'boy',
+    },
+    {
+        name: 'Julia', 
+        gender: 'girl',
+    },
+    {
+        name: 'Liam', 
+        gender: 'boy',
+    },
+    {
+        name: 'Erica', 
+        gender: 'girl',
+    },
+    {
+        name: 'Joey', 
+        gender: 'boy',
+    },
+    {
+        name: 'Rebecca', 
+        gender: 'girl',
+    },
+];
---
+++ script.js
@@ -0,0 +1,9 @@
+
+// Show new name
+function newName() {
+    const name = babyNames[Math.floor(Math.random() * babyNames.length)];
+    console.log(name);
+}
+
+// On Load
+newName();
